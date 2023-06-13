3e4356f1bf546c615ab3fc7727b77d3707160d82
Kevin Jue <kjue235@gmail.com>
2023-06-12 19:04:40 -0700

    added serialization function for affinepoint

--- src/serialization.rs
+++ src/serialization.rs
@@ -3,7 +3,9 @@ use plonky2::field::types::Field;
 use plonky2::util::serialization::{Buffer, IoResult, Read, Write};
 use plonky2_u32::serialization::{ReadU32, WriteU32};
 
+use crate::curve::curve_types::Curve;
 use crate::gadgets::biguint::BigUintTarget;
+use crate::gadgets::curve::AffinePointTarget;
 use crate::gadgets::nonnative::NonNativeTarget;
 
 pub trait WriteBigUint {
@@ -33,7 +35,7 @@ impl ReadBigUint for Buffer {
         let limbs = (0..length)
             .map(|_| self.read_target_u32())
             .collect::<Result<Vec<_>, _>>()?;
-        Ok(BigUintTarget{ limbs: limbs })
+        Ok(BigUintTarget{ limbs })
     }
 }
 
@@ -58,6 +60,33 @@ impl ReadNonNativeTarget for Buffer {
     #[inline]
     fn read_target_nonnative<FF:Field>(&mut self) -> IoResult<NonNativeTarget<FF>> {
         let value = self.read_target_biguint()?;
-        Ok(NonNativeTarget{ value: value, _phantom: core::marker::PhantomData })
+        Ok(NonNativeTarget{ value, _phantom: core::marker::PhantomData })
+    }
+}
+
+
+
+pub trait WriteAffinePoint {
+    fn write_target_affine_point<C: Curve>(&mut self, x: AffinePointTarget<C>) -> IoResult<()>;
+}
+
+impl WriteAffinePoint for Vec<u8> {
+    #[inline]
+    fn write_target_affine_point<C: Curve>(&mut self, point: AffinePointTarget<C>) -> IoResult<()> {
+        self.write_target_nonnative(point.x)?;
+        self.write_target_nonnative(point.y)
+    }
+}
+
+pub trait ReadAffinePoint {
+    fn read_target_affine_point<C: Curve>(&mut self) -> IoResult<AffinePointTarget<C>>;
+}
+
+impl ReadAffinePoint for Buffer {
+    #[inline]
+    fn read_target_affine_point<C: Curve>(&mut self) -> IoResult<AffinePointTarget<C>> {
+        let x = self.read_target_nonnative()?;
+        let y = self.read_target_nonnative()?;
+        Ok(AffinePointTarget{ x, y })
     }
 }
